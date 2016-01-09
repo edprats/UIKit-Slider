0f8860e8d8f017c756db00a38f47031e9e0e018c
Ed Prats <edprats@gmail.com>
2016-01-08 21:53:05 -0800

    A simple swift app where UIKit's slider is used to control a color view's background color

--- Color Sliders/ViewController.swift
+++ Color Sliders/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func changeOnSlider(sender: UISlider) {
+        let redValue = CGFloat(redSlider.value)
+        let blueValue = CGFloat(blueSlider.value)
+        let greenValue = CGFloat(greenSlider.value)
+        
+        print(redValue)
+        print(blueValue)
+        print(greenValue)
+        
+        colorView.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
+    }
+    
 }
 
